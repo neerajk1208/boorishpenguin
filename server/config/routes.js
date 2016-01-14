6c47838d94553489a6f174f5e11d3a8e86e9a15f
Brian Sweeney <bsweeney930@gmail.com>
2016-01-13 19:37:04 -0800

    user mod route edited to be able to modify a number of fields at once.

--- server/config/routes.js
+++ server/config/routes.js
@@ -22,7 +22,7 @@ module.exports = function(app, express) {
 
   app.get('/townhall/users', oAuth.ensureAuth, userControllers.allUsers);
   app.get('/townhall/users/:id', oAuth.ensureAuth, userControllers.oneUser);
-  app.post('/townhall/users/:id', oAuth.ensureAuth, userControllers.modUser); //used to modify users to be admins.
+  app.post('/townhall/users', oAuth.ensureAuth, userControllers.modUser); //used to modify users to be admins.
   app.get('/townhall/users/roles/:roleId', oAuth.ensureAuth, userControllers.readRole);
   app.post('/townhall/signup', userControllers.newUser);
 
